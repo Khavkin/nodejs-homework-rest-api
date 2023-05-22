f29f3f1e1c84b12bfd44450cf0c3abcae44a53f5
Khavkin <hdm@ukr.net>
2023-05-23 00:47:36 +0300

    ADD: joi,nanoid, validate.js. in contacts.js added functionality

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,74 @@
-const express = require('express')
+const express = require('express');
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require('../../models/contacts');
+const { schemaUpdate, schemaInsert } = require('../../utils/validate');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const data = await listContacts();
+    res.status(200).json({ data });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const data = await getContactById(req.params.contactId);
+    if (data.length > 0) res.status(200).json({ data });
+    else res.status(404).json({ message: 'Not found' });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const validationResult = schemaInsert.validate(req.body);
+
+    if (validationResult.error) {
+      return res.status(400).json({ message: validationResult.error.details[0].message });
+    }
+    const result = await addContact(req.body);
+    if (result) res.status(201).json({ data: result });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const result = await removeContact(req.params.contactId);
+    if (result >= 0) res.status(200).json({ message: 'contact deleted' });
+    else res.status(404).json({ message: 'Not found' });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    if (Object.keys(req.body).length !== 0) {
+      console.log('in if');
+      const validationResult = schemaUpdate.validate(req.body);
 
-module.exports = router
+      if (validationResult.error) {
+        return res.status(400).json({ message: validationResult.error.details[0].message });
+      }
+      const result = await updateContact(req.params.contactId, req.body);
+      if (result) res.status(200).json({ data: result });
+      else res.status(404).json({ message: 'Not found' });
+    } else res.status(400).json({ message: 'missing fields' });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+module.exports = router;
