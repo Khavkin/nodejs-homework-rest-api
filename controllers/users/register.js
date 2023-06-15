af4f39faa59ffc702921989831d154279ec9455a
Khavkin <hdm@ukr.net>
2023-06-16 00:14:45 +0300

    add avatarURL on register

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,18 +1,21 @@
 const bcrypt = require('bcrypt');
 const { usersService } = require('../../service');
 const { httpError } = require('../../helpers');
+const gravatar = require('gravatar');
 
 const register = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const saltRounds = 10;
+    const avatarURL = gravatar.url(email, { protocol: 'http', s: '250' });
 
     const hash = await bcrypt.hash(password, saltRounds);
-    console.log(hash);
+    //console.log(avatarUrl);
 
     const { email: responseEmail, subscription } = await usersService.register({
       email,
       password: hash,
+      avatarURL,
     });
 
     res.status(201).json({ user: { email: responseEmail, subscription } });
