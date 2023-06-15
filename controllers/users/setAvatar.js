d9f58ee936305faddb7c3046f74b091e1f8546b8
Khavkin <hdm@ukr.net>
2023-06-15 23:58:33 +0300

    create avatar name from user id

--- controllers/users/setAvatar.js
+++ controllers/users/setAvatar.js
@@ -2,23 +2,28 @@ const path = require('path');
 const { usersService } = require('../../service');
 const { httpError } = require('../../helpers');
 const fs = require('fs').promises;
+const jimp = require('jimp');
 
 const imageStorePath = path.join(process.cwd(), 'public', 'avatars');
 
 const setAvatar = async (req, res, next) => {
-  const { path: uploadName, originalname } = req.file;
+  const { path: uploadName, filename } = req.file;
   const { _id } = req.user;
 
-  const destFileName = path.join(imageStorePath, originalname);
+  console.log(req.file);
+
+  const destFileName = path.join(imageStorePath, filename);
   try {
     console.log(uploadName, destFileName);
+    const file = await jimp.read(uploadName);
+    file.resize(250, 250).write(uploadName);
     await fs.rename(uploadName, destFileName);
   } catch (err) {
     await fs.unlink(uploadName);
     return next(err);
   }
 
-  const avatarURL = 'avatars/' + originalname;
+  const avatarURL = 'avatars/' + filename;
   try {
     const user = await usersService.setAvatar(_id, avatarURL);
     res.status(200).json({ avatarURL });
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,17 +1,24 @@
 const multer = require('multer');
 const path = require('path');
+
 const uploadPath = path.join(process.cwd(), 'tmp');
+
+const ext = name => {
+  return name.match(/\.([^.]+)$/)?.[1];
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-
-    //console.log(file);
+    const { _id } = req.user;
+    //const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const fileExt = ext(file.originalname);
+    const fileName = _id + '.' + fileExt;
+    console.log(fileName);
     //cb(null, file.fieldname + '-' + uniqueSuffix);
-    cb(null, file.originalname);
+    cb(null, fileName);
   },
 });
 
